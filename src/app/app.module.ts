d9b93871be052d335882cf8e117af52ed7fb307b
Jenny Zhang <canaantt@gmail.com>
2018-04-16 11:01:22 -0700

    setting up framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { FilesComponent } from './files/files.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PermissionsComponent } from './permissions/permissions.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectDashboardComponent } from './project-dashboard/project-dashboard.component';
+import { RegisterComponent } from './register/register.component';
+import { UsersComponent } from './users/users.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    FilesComponent,
+    NavbarComponent,
+    PermissionsComponent,
+    ProjectsComponent,
+    ProjectDashboardComponent,
+    RegisterComponent,
+    UsersComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule
